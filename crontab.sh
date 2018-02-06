89681e63ccf6ce191723ad1b4531c4720b441ce6
Clyde Jones <slash5toaster@gmail.com>
2018-02-06 15:12:16 -0800

    2018-02-06-1512

--- crontab.sh
+++ crontab.sh
@@ -1,1 +1,8 @@
-7	6	*	*	*	cd ~/.kolmafia ; git pull gitssh master 2>&1 > /dev/null ; curl http://builds.kolmafia.us/KoLmafia-$(svn log -r HEAD -l 1  http://svn.code.sf.net/p/kolmafia/code/ | egrep '^r' | cut -d' ' -f1 | tr -d '[:alpha:]').jar -o KoLmafia-latest.jar ; git commit -a -m "updated jar $(svn log -r HEAD -l 1  http://svn.code.sf.net/p/kolmafia/code/ | egrep '^r' | cut -d' ' -f1 | tr -d '[:alpha:]')" ; git repack ; git push gitssh  &> /dev/null
+#!/bin/bash
+# 7	6	*	*	*
+cd ~/.kolmafia ;
+git pull master -p
+KM_VERSION=$(svn log -r HEAD -l 1  http://svn.code.sf.net/p/kolmafia/code/ | egrep '^r' | cut -d' ' -f1 | tr -d '[:alpha:]')
+curl http://builds.kolmafia.us/KoLmafia-${KM_VERSION}.jar -o KoLmafia-latest.jar
+git commit -a -m "updated jar $KM_VERSION
+git push
