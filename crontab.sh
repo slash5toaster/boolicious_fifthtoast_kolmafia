dd25c312b874120658ff129184334aa715c7f990
Clyde Jones <slash5toaster@gmail.com>
2018-12-04 23:35:16 -0800

    2018-12-04-2335

--- crontab.sh
+++ crontab.sh
@@ -5,6 +5,6 @@
 cd ~/.kolmafia ;
 git pull master -p
 export KM_VERSION=$(svn log -r HEAD -l 1  http://svn.code.sf.net/p/kolmafia/code/ | egrep '^r' | cut -d' ' -f1 | tr -d '[:alpha:]')
-curl https://ci.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/KoLmafia-${KM_VERSION}.jar -o KoLmafia-latest.jar && \
+curl https://ci.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/KoLmafia-${KM_VERSION}.jar  -o ~/.kolmafia/KoLmafia-latest.jar && \
 git commit -a -m "updated jar ${KM_VERSION}" && \
 git push
