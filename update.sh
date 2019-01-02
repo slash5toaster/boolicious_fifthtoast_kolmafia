ce59525f26dcb16cd20332a09e96eecb01b5d6ed
Clyde Jones <slash5toaster@gmail.com>
2019-01-01 16:57:14 -0800

    Update to 19057

--- update.sh
+++ update.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-cd ~/.kolmafia/
+#variables
+KM_ROOT=~/.kolmafia
+KM_BUILD_URL=https://ci.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/
+
+export KM_VERSION=$(svn log -r HEAD -l 1 \
+                    http://svn.code.sf.net/p/kolmafia/code/ | \
+                    egrep '^r' | cut -d' ' -f1 | tr -d '[:alpha:]') || exit 44
+
+#--------------------------------------------------
+cd $KM_ROOT/
 git pull --all -p
 
-export KM_VERSION=$(svn log -r HEAD -l 1  http://svn.code.sf.net/p/kolmafia/code/ | \
-                    egrep '^r' | cut -d' ' -f1 | tr -d '[:alpha:]') || exit 44
-curl https://ci.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/KoLmafia-${KM_VERSION}.jar \
-     -o ~/.kolmafia/KoLmafia-latest.jar
+if [[ $(cat $KM_ROOT/km_version) == $KM_VERSION ]]; then
+  echo "no update required"
+else
+  curl $KM_BUILD_URL/KoLmafia-${KM_VERSION}.jar \
+       -o $KM_ROOT/KoLmafia-latest.jar
+fi
+
 if [[ $(git status --porcelain --untracked-files=no | wc -l) -gt 0 ]] ; then
   git add KoLmafia-latest.jar
   git commit -m "Update to $KM_VERSION"
+  echo $KM_VERSION | tee $KM_ROOT/km_version
 fi
