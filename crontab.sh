82d41a3bf52de92df3c89374042c6ac452cbdb94
Clyde Jones <slash5toaster@gmail.com>
2019-01-01 16:47:07 -0800

    updated jar 19057

--- crontab.sh
+++ crontab.sh
@@ -2,9 +2,9 @@
 # 7	6	*	*	*
 #curl http://builds.kolmafia.us/KoLmafia-${KM_VERSION}.jar -o KoLmafia-latest.jar
 #curl http://builds.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/KoLmafia-${KM_VERSION}.jar -o KoLmafia-latest.jar
-cd ~/.kolmafia ;
-git pull master -p
-export KM_VERSION=$(svn log -r HEAD -l 1  http://svn.code.sf.net/p/kolmafia/code/ | egrep '^r' | cut -d' ' -f1 | tr -d '[:alpha:]')
-curl https://ci.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/KoLmafia-${KM_VERSION}.jar  -o ~/.kolmafia/KoLmafia-latest.jar && \
-git commit -a -m "updated jar ${KM_VERSION}" && \
-git push
+7	6	*	*	* cd ~/.kolmafia ; \
+          git pull --all -p ; \
+          export KM_VERSION=$(svn log -r HEAD -l 1  http://svn.code.sf.net/p/kolmafia/code/ | egrep '^r' | cut -d' ' -f1 | tr -d '[:alpha:]') && \
+          curl https://ci.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/KoLmafia-${KM_VERSION}.jar  -o ~/.kolmafia/KoLmafia-latest.jar && \
+          git commit -a -m "updated jar ${KM_VERSION}" && \
+          git push
--- update.sh
+++ update.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+cd ~/.kolmafia/
+git pull --all -p
+
 export KM_VERSION=$(svn log -r HEAD -l 1  http://svn.code.sf.net/p/kolmafia/code/ | \
                     egrep '^r' | cut -d' ' -f1 | tr -d '[:alpha:]') || exit 44
 curl https://ci.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/KoLmafia-${KM_VERSION}.jar \
      -o ~/.kolmafia/KoLmafia-latest.jar
-cd ~/.kolmafia/
 if [[ $(git status --porcelain --untracked-files=no | wc -l) -gt 0 ]] ; then
   git add KoLmafia-latest.jar
   git commit -m "Update to $KM_VERSION"
